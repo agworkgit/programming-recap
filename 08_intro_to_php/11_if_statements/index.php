c8eb88ce77921eb6ed522bb5b6252ab69f0f6662
Casian Alexandru Grigore <agworksbox@gmail.com>
2026-08-30 09:07:58 +0100

    oma: intro to php - if/else

---
+++ 08_intro_to_php/11_if_statements/index.php
@@ -0,0 +1,9 @@
+<?php
+$age = 16;
+
+print "Your age is $age.";
+print "\n";
+
+if ($age < 18) {
+    print "You're too young!";
+}
---
+++ 08_intro_to_php/12_if_else_statements/index.php
@@ -0,0 +1,7 @@
+<?php
+
+if (true) {
+    print "It's TRUE";
+} else {
+    print "It's NOT TRUE";
+}
