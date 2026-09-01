1f200d3d396b34f588d0dc8dc440f43d64dc3f2f
Casian Alexandru Grigore <agworksbox@gmail.com>
2026-09-01 23:07:13 +0100

    oma: php - glob

--- 08_intro_to_php/36_glob_list_filter_files/glob.php
+++ 08_intro_to_php/36_glob_list_filter_files/glob.php
@@ -1,2 +1,38 @@
 <?php
 // Allows you to read all the contents in a folder, and then filter them
+// You can also use the media files in HTML tags to display a lot of image files, audio files, pdfs etc...very useful!
+// You can also use it to create a search across files and display what's being searched.
+$glob = glob("./*");
+
+print_r($glob);
+
+echo "\n";
+
+foreach ($glob as $file) {
+    echo "{$file}\n";
+}
+
+/* 
+OUTPUT:
+
+Array
+(
+    [0] => ./01_basic_js
+    [1] => ./02_head_first_js
+    [2] => ./03_think_like_a_programmer
+    [3] => ./05_modern_cpp
+    [4] => ./06_python_crash
+    [5] => ./07_missing_semester
+    [6] => ./08_intro_to_php
+    [7] => ./readme.md
+)
+
+./01_basic_js
+./02_head_first_js
+./03_think_like_a_programmer
+./05_modern_cpp
+./06_python_crash
+./07_missing_semester
+./08_intro_to_php
+./readme.md
+*/
